0adb4f8f425ba4d379fb5d197e8932ab21e1921b
Aaron Sawyer <me@aarondevon.com>
2019-07-02 11:33:08 -0500

    add helper function to create drum arrays

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,17 @@
+// Helper function to create drum arrays
+const arrayInitialToFalse = (numberOfIndexes) => {
+	let array = [];
+	for (let i = 0; i < numberOfIndexes; i++) {
+		array.push(false);
+	}
+	return array;
+};
+
 // Drum Arrays
-let kicks
-let snares
-let hiHats
-let rideCymbals
+let kicks = arrayInitialToFalse(16);
+let snares = arrayInitialToFalse(16);
+let hiHats = arrayInitialToFalse(16);
+let rideCymbals = arrayInitialToFalse(16);
+
+
 
